c94a54cf8b6b7bc99b5b90797407cbb8f6419b6f
kyle.rb <kyleryanboyle@gmail.com>
2017-12-11 03:07:49 -0500

    moved demo to readme and fixed some things

--- simplecopy.js
+++ simplecopy.js
@@ -1,33 +1,36 @@
-var node = document.createElement("DIV");
-node.style.position = "fixed"; // doesn't interact with the DOM
-node.style.color = "rgba(0,0,0,0)"; // transparent
-node.innerHTML = copyText;
+var simplecopy = function() { // make a closure so we don't pollute the namespace
+    var node = document.createElement("DIV");
+    node.style.position = "fixed"; // doesn't interact with the DOM
+    node.style.color = "rgba(0,0,0,0)"; // transparent
+    node.innerHTML = copyText;
 
-function simpleCopy(copyText, callback) {
-    document.body.appendChild(node);
-    var selectStatus = selectText(node);
-    var copyStatus = document.execCommand("copy", false, null); // copies currently selected text
-    document.body.removeChild(node);
-    callback(selectStatus && copyStatus);
-}
+    function simplecopy(copyText, callback) {
+        document.body.appendChild(node);
+        var selectStatus = selectText(node);
+        var copyStatus = document.execCommand("copy", false, null); // copy currently selected text
+        document.body.removeChild(node);
+        return selectStatus && copyStatus;
+    }
 
-function selectText(node) {
-    var range, selection;
-    if (document.body.createTextRange) { // if the browser supports this old thing
-        range = document.body.createTextRange(); // create a text range
-        range.moveToElementText(node);
-        range.select();
-        return true;
+    function selectText(node) {
+        var range, selection;
+        if (document.body.createTextRange) { // if the browser supports this old method
+            range = document.body.createTextRange(); // create a text range
+            range.moveToElementText(node);
+            range.select();
+            return true;
+        }
+        else if (window.getSelection) { // if the browser supports the newer method
+            selection = window.getSelection(); // get a selection object
+            range = document.createRange(); // create a blank range object
+            range.selectNodeContents(node);
+            selection.removeAllRanges(); // clear any current range from the selection
+            selection.addRange(range); // select the contents of the range
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-    else if (window.getSelection) { // if the browser supports the newer thing (most of the time)
-        selection = window.getSelection(); // get a selection object
-        range = document.createRange(); // create a blank range object
-        range.selectNodeContents(node);
-        selection.removeAllRanges(); // clear any current range from the selection
-        selection.addRange(range); // select the contents of the range
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+    return simpleCopy;
+}();
